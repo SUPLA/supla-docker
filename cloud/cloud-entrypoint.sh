1451c71cc8ed30b83a25ac9b309452ec0cf36941
Wojciech Frącz <fraczwojciech@gmail.com>
2017-12-14 19:43:50 +0100

    Add missing envs, add reasonable defaults

--- cloud/cloud-entrypoint.sh
+++ cloud/cloud-entrypoint.sh
@@ -14,10 +14,10 @@ sed -i "s+ewz_recaptcha_public_key: ~+ewz_recaptcha_public_key: ${RECAPTCHA_PUBL
 sed -i "s+ewz_recaptcha_private_key: ~+ewz_recaptcha_private_key: ${RECAPTCHA_PRIVATE_KEY:-~}+g" app/config/parameters.yml
 
 sed -i "s+mailer_host: 127.0.0.1+mailer_host: ${MAILER_HOST:-127.0.0.1}+g" app/config/parameters.yml
-sed -i "s+mailer_user: ~+mailer_host: ${MAILER_USER:-~}+g" app/config/parameters.yml
+sed -i "s+mailer_user: ~+mailer_user: ${MAILER_USER:-~}+g" app/config/parameters.yml
 sed -i "s+mailer_password: ~+mailer_password: ${MAILER_PASSWORD:-~}+g" app/config/parameters.yml
-sed -i "s+mailer_port: 465+mailer_port: ${MAILER_PASSWORD:-465}+g" app/config/parameters.yml
-sed -i "s+mailer_encryption: ssl+mailer_encryption: ${MAILER_ENCRYPTION:-ssl}+g" app/config/parameters.yml
+sed -i "s+mailer_port: 465+mailer_port: ${MAILER_PORT:-25}+g" app/config/parameters.yml
+sed -i "s+mailer_encryption: ssl+mailer_encryption: ${MAILER_ENCRYPTION:-~}+g" app/config/parameters.yml
 sed -i "s+mailer_from: ~+mailer_from: ${MAILER_FROM:-~}+g" app/config/parameters.yml
 sed -i "s+admin_email: ~: ~+admin_email: ${ADMIN_EMAIL:-~}+g" app/config/parameters.yml
 
