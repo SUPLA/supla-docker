3d201ebe797c6376726994b2afae2fd21c940c67
Wojciech Frącz <fraczwojciech@gmail.com>
2018-03-29 15:46:29 +0200

    Support new parameters in docker config

--- cloud/cloud-entrypoint.sh
+++ cloud/cloud-entrypoint.sh
@@ -10,8 +10,8 @@ sed -i "s+secret: ThisTokenIsNotSoSecretChangeIt+secret: ${SECRET:-DEFAULT_SECRE
 sed -i "s+supla_server: ~+supla_server: ${CLOUD_DOMAIN:-cloud.supla.org}+g" app/config/parameters.yml
 
 sed -i "s+recaptcha_enabled: true+recaptcha_enabled: ${RECAPTCHA_ENABLED:-true}+g" app/config/parameters.yml
-sed -i "s+ewz_recaptcha_public_key: ~+ewz_recaptcha_public_key: ${RECAPTCHA_PUBLIC_KEY:-~}+g" app/config/parameters.yml
-sed -i "s+ewz_recaptcha_private_key: ~+ewz_recaptcha_private_key: ${RECAPTCHA_PRIVATE_KEY:-~}+g" app/config/parameters.yml
+sed -i "s+recaptcha_site_key: ~+recaptcha_site_key: ${RECAPTCHA_PUBLIC_KEY:-~}+g" app/config/parameters.yml
+sed -i "s+recaptcha_secret: ~+recaptcha_secret: ${RECAPTCHA_PRIVATE_KEY:-~}+g" app/config/parameters.yml
 
 sed -i "s+mailer_host: 127.0.0.1+mailer_host: ${MAILER_HOST:-127.0.0.1}+g" app/config/parameters.yml
 sed -i "s+mailer_user: ~+mailer_user: ${MAILER_USER:-~}+g" app/config/parameters.yml
@@ -21,6 +21,9 @@ sed -i "s+mailer_encryption: ssl+mailer_encryption: ${MAILER_ENCRYPTION:-~}+g" a
 sed -i "s+mailer_from: ~+mailer_from: ${MAILER_FROM:-~}+g" app/config/parameters.yml
 sed -i "s+admin_email: ~+admin_email: ${ADMIN_EMAIL:-~}+g" app/config/parameters.yml
 sed -i "s+supla_protocol: https+supla_protocol: ${SUPLA_PROTOCOL:-https}+g" app/config/config.yml
+
+sed -i "s+supla_require_regulations_acceptance: false+supla_require_regulations_acceptance: ${REQUIRE_REGULATIONS_ACCEPTANCE:-false}+g" app/config/config.yml
+sed -i "s+supla_require_cookie_policy_acceptance: false+supla_require_cookie_policy_acceptance: ${REQUIRE_COOKIE_POLICY_ACCEPTANCE:-false}+g" app/config/config.yml
 
 rm -fr var/cache/*
 php bin/console supla:initialize
