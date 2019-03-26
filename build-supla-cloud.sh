89268800b67ecfda2b84ca7e1ac5db3c38d07522
Wojciech Frącz <fraczwojciech@gmail.com>
2019-03-26 22:54:30 +0100

    Do not continue releasing if any command has failed

--- build-supla-cloud.sh
+++ build-supla-cloud.sh
@@ -21,21 +21,21 @@ LAST_DEVELOP_VERSION=$(docker exec -it -u www-data supla-cloud-builder git descr
 CURRENT_DEVELOP_VERSION=$(docker exec -it -u www-data supla-cloud-builder git describe --tags origin/develop)
 
 if [ $LAST_MASTER_VERSION != $CURRENT_MASTER_VERSION ]; then
-    docker exec -it -u www-data supla-cloud-builder git checkout -f master
-    docker exec -it -u www-data supla-cloud-builder git pull
-    docker exec -it -u www-data supla-cloud-builder git fetch origin develop:develop
-    docker exec -it -u www-data --env RELEASE_FILENAME=supla-cloud-master.tar.gz supla-cloud-builder composer run-script release
-    docker cp supla-cloud-builder:/var/www/supla-cloud/supla-cloud-master.tar.gz cloud/supla-cloud-develop.tar.gz
-    ./supla.sh restart
-    sleep 5
+    docker exec -it -u www-data supla-cloud-builder git checkout -f master && \
+    docker exec -it -u www-data supla-cloud-builder git pull && \
+    docker exec -it -u www-data supla-cloud-builder git fetch origin develop:develop && \
+    docker exec -it -u www-data --env RELEASE_FILENAME=supla-cloud-master.tar.gz supla-cloud-builder composer run-script release && \
+    docker cp supla-cloud-builder:/var/www/supla-cloud/supla-cloud-master.tar.gz cloud/supla-cloud-develop.tar.gz && \
+    ./supla.sh restart && \
+    sleep 5 && \
     docker cp cloud/supla-cloud-develop.tar.gz supla-cloud:/var/www/cloud/web/supla-cloud-master.tar.gz
 elif [ $LAST_DEVELOP_VERSION != $CURRENT_DEVELOP_VERSION ]; then
-    docker exec -it -u www-data supla-cloud-builder git checkout -f develop
-    docker exec -it -u www-data supla-cloud-builder git pull
-    docker exec -it -u www-data --env RELEASE_FILENAME=supla-cloud-develop.tar.gz --env RELEASE_VERSION=$CURRENT_DEVELOP_VERSION supla-cloud-builder composer run-script release
-    docker cp supla-cloud-builder:/var/www/supla-cloud/supla-cloud-develop.tar.gz cloud/supla-cloud-develop.tar.gz
-    ./supla.sh restart
-    sleep 5
+    docker exec -it -u www-data supla-cloud-builder git checkout -f develop && \
+    docker exec -it -u www-data supla-cloud-builder git pull && \
+    docker exec -it -u www-data --env RELEASE_FILENAME=supla-cloud-develop.tar.gz --env RELEASE_VERSION=$CURRENT_DEVELOP_VERSION supla-cloud-builder composer run-script release && \
+    docker cp supla-cloud-builder:/var/www/supla-cloud/supla-cloud-develop.tar.gz cloud/supla-cloud-develop.tar.gz && \
+    ./supla.sh restart && \
+    sleep 5 && \
     docker cp cloud/supla-cloud-develop.tar.gz supla-cloud:/var/www/cloud/web/supla-cloud-develop.tar.gz
 fi
 
