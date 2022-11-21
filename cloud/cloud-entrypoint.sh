4356ffb01d65b694149678a10a5295daaf53f3a1
Wojciech Frącz <fraczwojciech@gmail.com>
2022-11-21 13:47:02 +0100

    Write full parameters.yml in cloud entrypoint

--- cloud/cloud-entrypoint.sh
+++ cloud/cloud-entrypoint.sh
@@ -5,33 +5,39 @@ if [ ! -f /etc/apache2/ssl/server.crt ]; then
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/apache2/ssl/server.key -out /etc/apache2/ssl/server.crt -subj "/C=PL/ST=SUPLA/L=SUPLA/O=SUPLA/CN=SUPLA"
 fi
 
-sed -i "s+database_host: 127.0.0.1+database_host: ${DB_HOST:-supla-db}+g" app/config/parameters.yml
-sed -i "s+database_port: null+database_port: ${DB_PORT:-null}+g" app/config/parameters.yml
-sed -i "s+database_name: supla+database_name: ${DB_NAME:-supla}+g" app/config/parameters.yml
-sed -i "s+database_user: root+database_user: ${DB_USER:-supla}+g" app/config/parameters.yml
-sed -i "s+database_password: ~+database_password: ${DB_PASSWORD:-DEFAULT_PASSWORD_IS_BAD_IDEA}+g" app/config/parameters.yml
+echo "
+parameters:
+    database_driver: pdo_mysql
+    database_host: ${DB_HOST:-supla-db}
+    database_port: ${DB_PORT:-null}
+    database_name: ${DB_NAME:-supla}
+    database_user: ${DB_USER:-supla}
+    database_password: ${DB_PASSWORD:-DEFAULT_PASSWORD_IS_BAD_IDEA}
+    mailer_transport: smtp
+    mailer_host: ${MAILER_HOST:-127.0.0.1}
+    mailer_user: ${MAILER_USER:-~}
+    mailer_password: ${MAILER_PASSWORD:-~}
+    mailer_port: ${MAILER_PORT:-25}
+    mailer_encryption: ${MAILER_ENCRYPTION:-~}
+    mailer_from: ${MAILER_FROM:-~}
+    admin_email: ${ADMIN_EMAIL:-~}
+    supla_server: ${CLOUD_DOMAIN:-cloud.supla.org}
+    supla_require_regulations_acceptance: ${REQUIRE_REGULATIONS_ACCEPTANCE:-false}
+    supla_require_cookie_policy_acceptance: ${REQUIRE_COOKIE_POLICY_ACCEPTANCE:-false}
+    brute_force_auth_prevention_enabled: ${BRUTE_FORCE_AUTH_PREVENTION_ENABLED:-true}
+    recaptcha_enabled: ${RECAPTCHA_ENABLED:-false}
+    recaptcha_site_key: ${RECAPTCHA_PUBLIC_KEY:-~}
+    recaptcha_secret: ${RECAPTCHA_PRIVATE_KEY:-~}
+    locale: en
+    secret: ${SECRET:-DEFAULT_SECRET_IS_BAD_IDEA}
+    cors_allow_origin_regex:
+        - supla2.+
+        - localhost.+
+" > app/config/parameters.yml
 
-sed -i "s+secret: ThisTokenIsNotSoSecretChangeIt+secret: ${SECRET:-DEFAULT_SECRET_IS_BAD_IDEA}+g" app/config/parameters.yml
-sed -i "s+supla_server: ~+supla_server: ${CLOUD_DOMAIN:-cloud.supla.org}+g" app/config/parameters.yml
-
-sed -i "s+recaptcha_enabled: false+recaptcha_enabled: ${RECAPTCHA_ENABLED:-false}+g" app/config/parameters.yml
-sed -i "s+recaptcha_site_key: ~+recaptcha_site_key: ${RECAPTCHA_PUBLIC_KEY:-~}+g" app/config/parameters.yml
-sed -i "s+recaptcha_secret: ~+recaptcha_secret: ${RECAPTCHA_PRIVATE_KEY:-~}+g" app/config/parameters.yml
-
-sed -i "s+mailer_host: 127.0.0.1+mailer_host: ${MAILER_HOST:-127.0.0.1}+g" app/config/parameters.yml
-sed -i "s+mailer_user: ~+mailer_user: ${MAILER_USER:-~}+g" app/config/parameters.yml
-sed -i "s+mailer_password: ~+mailer_password: ${MAILER_PASSWORD:-~}+g" app/config/parameters.yml
-sed -i "s+mailer_port: 465+mailer_port: ${MAILER_PORT:-25}+g" app/config/parameters.yml
-sed -i "s+mailer_encryption: ssl+mailer_encryption: ${MAILER_ENCRYPTION:-~}+g" app/config/parameters.yml
-sed -i "s+mailer_from: ~+mailer_from: ${MAILER_FROM:-~}+g" app/config/parameters.yml
-sed -i "s+admin_email: ~+admin_email: ${ADMIN_EMAIL:-~}+g" app/config/parameters.yml
 sed -i "s+supla_protocol: https+supla_protocol: ${SUPLA_PROTOCOL:-https}+g" app/config/config.yml
 sed -E -i "s@supla_url: '(.+)'@supla_url: '${SUPLA_URL:-\1}'@g" app/config/config.yml
 sed -i "s+accounts_registration_enabled: true+accounts_registration_enabled: ${ACCOUNTS_REGISTRATION_ENABLED:-true}+g" app/config/config.yml
-
-sed -i "s+supla_require_regulations_acceptance: false+supla_require_regulations_acceptance: ${REQUIRE_REGULATIONS_ACCEPTANCE:-false}+g" app/config/parameters.yml
-sed -i "s+supla_require_cookie_policy_acceptance: false+supla_require_cookie_policy_acceptance: ${REQUIRE_COOKIE_POLICY_ACCEPTANCE:-false}+g" app/config/parameters.yml
-sed -i "s+brute_force_auth_prevention_enabled: true+brute_force_auth_prevention_enabled: ${BRUTE_FORCE_AUTH_PREVENTION_ENABLED:-true}+g" app/config/parameters.yml
 
 if ! grep -q mqtt app/config/config_build.yml; then
   echo "    mqtt_broker:
