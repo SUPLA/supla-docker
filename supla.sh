bb0ed0e340ab19f3a373ddb76e9a988782310dd7
Wojciech Frącz <fraczwojciech@gmail.com>
2026-05-12 12:24:18 +0200

    Rewrite supla.sh script

--- supla.sh
+++ supla.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 cd "$(dirname "$0")"
 
 RED='\033[0;31m'
@@ -7,73 +9,110 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
 
-if command -v docker-compose >/dev/null 2>&1; then
-  DOCKER_COMPOSE="docker-compose"
-elif docker compose version >/dev/null 2>&1; then
-  DOCKER_COMPOSE="docker compose"
+if docker compose version >/dev/null 2>&1; then
+  DOCKER_COMPOSE=(docker compose)
+elif command -v docker-compose >/dev/null 2>&1; then
+  DOCKER_COMPOSE=(docker-compose)
 else
-  echo -e "${RED}Neither docker-compose nor docker compose found. Please install Docker Compose.${NC}"
+  echo -e "${RED}Neither docker compose nor docker-compose found. Please install Docker Compose.${NC}"
   exit 1
 fi
 
-function docker-compose() {
-  $DOCKER_COMPOSE "$@"
+docker_compose() {
+  "${DOCKER_COMPOSE[@]}" "$@"
 }
 
-if [ ! -f .env ]; then
-  cp .env.default .env
-  DB_PASSWORD="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
-  APP_SECRET="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)"
-  sed -i "s+CHANGE_PASSWORD_BEFORE_FIRST_LAUNCH+$DB_PASSWORD+g" .env
-  sed -i "s+CHANGE_SECRET_BEFORE_FIRST_LAUNCH+$SECRET+g" .env
-  echo -e "${YELLOW}Sample configuration file has been generated for you.${NC}"
-  echo -e "${YELLOW}Please check if the .env file matches your needs and run this command again.${NC}"
-  exit
-fi
+generate_secret() {
+  tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c "$1"
+}
 
-source .env >/dev/null 2>&1
+ensure_env_file() {
+  if [ ! -f .env ]; then
+    cp .env.default .env
 
-if [ "${MAILER_HOST}" != "" ]; then
-  echo -e "${YELLOW}[WARN] You are using deprecated e-mail configuration.${NC}"
-  echo -e "${YELLOW}[WARN] Please use MAILER_DSN environment variable to configure it.${NC}"
-  echo -e "${YELLOW}[WARN] See .env.default for examples.${NC}"
-fi
+    DB_PASSWORD="$(generate_secret 32)"
+    APP_SECRET="$(generate_secret 64)"
 
-if [ -z "$DB_IMAGE" ]; then
-  DB_IMAGE="mysql:5.7.20"
-fi
+    sed -i "s+CHANGE_PASSWORD_BEFORE_FIRST_LAUNCH+$DB_PASSWORD+g" .env
+    sed -i "s+CHANGE_SECRET_BEFORE_FIRST_LAUNCH+$APP_SECRET+g" .env
 
-if [ "$DB_IMAGE" = "mysql:5.7.20" ]; then
-  echo -e "${YELLOW}Using the outdated MySQL image 5.7.20.${NC}"
-  echo -e "${YELLOW}Please consider upgrading your SUPLA stack.${NC}"
-  echo -e "${YELLOW}The support for current configuration will be dropped at the end of 2025.${NC}"
-  echo -e "${YELLOW}See https://github.com/SUPLA/supla-docker/wiki/Database-upgrade-v25.04 for more information.${NC}"
-  if [ "$(expr substr $(dpkg --print-architecture) 1 3)" == "arm" ]; then
-    DB_IMAGE="hypriot/rpi-mysql:5.5"
+    echo -e "${YELLOW}Sample configuration file has been generated for you.${NC}"
+    echo -e "${YELLOW}Please check if the .env file matches your needs and run this command again.${NC}"
+    exit 0
   fi
-fi
+}
 
-# remove \r at the end of the env, if exists
-CONTAINER_NAME="$(echo -e "${COMPOSE_PROJECT_NAME}" | sed -e 's/\r$//')"
+load_env_file() {
+  set -a
+  # shellcheck disable=SC1091
+  source .env
+  set +a
+}
 
-if [ "$1" = "start" ]; then
-  echo -e "${GREEN}Starting SUPLA containers${NC}" && \
-  docker-compose up --build -d && \
-  echo -e "${GREEN}SUPLA containers has been started.${NC}"
+validate_required_env() {
+  if [ -z "${DATABASE_IMAGE:-}" ]; then
+    echo -e "${RED}DATABASE_IMAGE is not set. Please define it in .env.${NC}"
+    exit 1
+  fi
+}
 
-elif [ "$1" = "stop" ]; then
+prepare_env() {
+  if [ -n "${MAILER_HOST:-}" ]; then
+    echo -e "${YELLOW}[WARN] You are using deprecated e-mail configuration.${NC}"
+    echo -e "${YELLOW}[WARN] Please use MAILER_DSN environment variable to configure it.${NC}"
+    echo -e "${YELLOW}[WARN] See .env.default for examples.${NC}"
+  fi
+}
+
+start() {
+  echo -e "${GREEN}Starting SUPLA containers${NC}"
+  docker_compose up --build -d
+  echo -e "${GREEN}SUPLA containers have been started.${NC}"
+}
+
+stop() {
   echo -e "${GREEN}Stopping SUPLA containers${NC}"
-  docker-compose stop && echo -e "${GREEN}SUPLA containers has been stopped.${NC}"
+  docker_compose stop
+  echo -e "${GREEN}SUPLA containers have been stopped.${NC}"
+}
 
-elif [ "$1" = "restart" ]; then
-  "./$(basename "$0")" stop
+restart() {
+  stop
   sleep 1
-  "./$(basename "$0")" start
+  start
+}
 
-elif [ "$1" = "create-confirmed-user" ]; then
-  docker exec -it -u www-data "$CONTAINER_NAME-cloud" php bin/console supla:create-confirmed-user
+upgrade() {
+  echo -e "${GREEN}Updating SUPLA containers${NC}"
+  docker_compose build --pull
+  docker_compose up -d --remove-orphans
+  echo -e "${GREEN}SUPLA containers have been updated.${NC}"
+}
 
-else
-  echo -e "${RED}Usage: $0 start|stop|restart|create-confirmed-user${NC}"
+create_confirmed_user() {
+  docker_compose exec -u www-data supla-cloud php bin/console supla:create-confirmed-user
+}
 
-fi
+console() {
+  shift
+  docker_compose exec -u www-data supla-cloud php bin/console "$@"
+}
+
+usage() {
+  echo -e "${RED}Usage: $0 start|stop|restart|update|create-confirmed-user${NC}"
+}
+
+ensure_env_file
+load_env_file
+validate_required_env
+prepare_env
+
+case "${1:-}" in
+  start) start ;;
+  stop) stop ;;
+  restart) restart ;;
+  upgrade) upgrade ;;
+  console) console "$@" ;;
+  create-confirmed-user) create_confirmed_user ;;
+  *) usage; exit 1 ;;
+esac
